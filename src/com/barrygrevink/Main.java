f63ae46c24ebfac6f47caf426dfdc15288477145
barrygrevink <barrygrevink1985@gmail.com>
2019-12-08 14:26:16 +0100

    Case Switching Combined With Else

---
+++ src/com/barrygrevink/Main.java
@@ -0,0 +1,78 @@
+package com.barrygrevink;
+
+public class Main {
+
+    public static void printDayOfTheWeek(int day) {
+
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Error input day");
+                break;
+
+        }
+
+    }
+
+    public static void printDayIf(int day) {
+        if (day == 0) {
+            System.out.println("Sunday");
+        } else if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else {
+            System.out.println("Error input day");
+        }
+    }
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(10);
+
+        printDayIf(0);
+        printDayIf(1);
+        printDayIf(2);
+        printDayIf(3);
+        printDayIf(4);
+        printDayIf(5);
+        printDayIf(6);
+        printDayIf(10);
+
+
+    }
+}
